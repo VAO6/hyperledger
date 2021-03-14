cbaef5283fc84368df96cb34a352b93719134e70
VAO6 <varisti6@gmail.com>
2021-03-14 23:48:04 +0000

    configuracion red docker de peers, orderer, cli, couchdbs

--- scripts/prerreq.sh
+++ scripts/prerreq.sh
@@ -13,32 +13,32 @@ echo "####################################################### "
 echo "#INSTALLING UTILS# "
 echo "####################################################### "
 #unzip
-sudo apt install unzip -y
+brew install unzip
 #wget
-sudo apt install wget  -y
+brew install wget 
 #wget
-sudo apt install git -y
+brew install git
 #tree
-sudo apt install tree -y
+brew install tree
 #telnet
-sudo apt-get install telnetd -y
+brew install telnetd
 #httping
-sudo apt-get update -y
-sudo apt-get install -y httping
+brew update
+brew install curl
+brew install httping
 #tcpping
-sudo apt-get install tcptraceroute -y
+brew install tcptraceroute
 cd /usr/bin/
-wget http://pingpros.com/pub/tcpping
-chmod 755 tcpping
-sudo apt-get install hping3 -y
+# wget http://pingpros.com/pub/tcpping
+# chmod 755 tcpping
+# brew install hping3
 
 #JAVA
 echo "####################################################### "
 echo "#                   INSTALLING JAVA                  # "
 echo "####################################################### "
-sudo apt install openjdk-8-jre-headless -y
-echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> ~/.bashrc
-echo 'export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"' >> ~/.profile
+brew install openjdk-8-jre-headless
+echo 'export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"' >> ~/.bash_profile
 echo "Java version ..."
 java -version
 
@@ -51,104 +51,92 @@ cd /tmp
 wget https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip
 unzip apache-maven-3.6.3-bin.zip
 sudo mv apache-maven-3.6.3/* /opt/apache-maven/
-echo 'export M2_HOME=/opt/apache-maven' >> ~/.bashrc
-echo 'export PATH=${M2_HOME}/bin:${PATH}' >> ~/.bashrc
+echo 'export M2_HOME=/opt/apache-maven' >> ~/.bash_profile
+echo 'export PATH=$M2_HOME/bin:$PATH' >> ~/.bash_profile
 
-echo 'export M2_HOME=/opt/apache-maven' >> ~/.profile
-echo 'export PATH=${M2_HOME}/bin:${PATH}' >> ~/.profile
 echo "MAVEN version ..."
 #mvn -v
-#DOCKER
-echo "        ####################################################### "
-echo "        #                   INSTALLING DOCKER                  # "
-echo "        ####################################################### "
-sudo apt-get update -y
+# #DOCKER
+# echo "        ####################################################### "
+# echo "        #                   INSTALLING DOCKER                  # "
+# echo "        ####################################################### "
+# brew update
 
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+# brew-get install \
+#     apt-transport-https \
+#     ca-certificates \
+#     curl \
+#     gnupg-agent \
+#     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo apt-key fingerprint 0EBFCD88
+# sudo apt-key fingerprint 0EBFCD88
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+# sudo add-apt-repository \
+#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#    $(lsb_release -cs) \
+#    stable"
 
-sudo apt-get update -y
+# sudo apt-get update
 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io -y
+# sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-sudo docker run hello-world
+# sudo docker run hello-world
 
-#fix docker deamon
-echo "        ####################################################### "
-echo "        #                   FIXING DOCKER                  # "
-echo "        ####################################################### "
-cd /home
-mkdir $USER/.docker
-sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
-sudo chmod g+rwx "$HOME/.docker" -R
-sudo chmod 666 /var/run/docker.sock
-test the fix
-docker run hello-world
+# #fix docker deamon
+# echo "        ####################################################### "
+# echo "        #                   FIXING DOCKER                  # "
+# echo "        ####################################################### "
+# cd /home
+# mkdir $USER/.docker
+# sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+# sudo chmod g+rwx "$HOME/.docker" -R
+# sudo chmod 666 /var/run/docker.sock
+# test the fix
+# docker run hello-world
 
 
-#DOCKER_COMPOSE
-echo "        ####################################################### "
-echo "        #                   INSTALLING DOCKER COMPOSE         # "
-echo "        ####################################################### "
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#Apply executable permissions to the binary
-sudo chmod +x /usr/local/bin/docker-compose
-echo "DOCKER version ..."
-docker version
-echo "DOCKER COMPOSE version ..."
-#docker-compose version
+# #DOCKER_COMPOSE
+# echo "        ####################################################### "
+# echo "        #                   INSTALLING DOCKER COMPOSE         # "
+# echo "        ####################################################### "
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# #Apply executable permissions to the binary
+# sudo chmod +x /usr/local/bin/docker-compose
+# echo "DOCKER version ..."
+# docker version
+# echo "DOCKER COMPOSE version ..."
+# #docker-compose version
 
-#NODE
-echo "        ####################################################### "
-echo "        #                   INSTALLING NODE                  # "
-echo "        ####################################################### "
+# #NODE
+# echo "        ####################################################### "
+# echo "        #                   INSTALLING NODE                  # "
+# echo "        ####################################################### "
 
-#  Node.js 8.x LTS Carbon is no longer actively supported!
-curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
-sudo apt install nodejs -y
-echo "NODE version ..."
-node --version
-echo "NPM version ..."
-npm --version
+# #  Node.js 8.x LTS Carbon is no longer actively supported!
+# curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+# sudo apt install nodejs
+# echo "NODE version ..."
+# node --version
+# echo "NPM version ..."
+# npm --version
 
 
 #GO
 echo "        ####################################################### "
 echo "        #                   INSTALLING GOLANG                  # "
 echo "        ####################################################### "
-sudo apt update -y
+# sudo apt update
 cd /tmp
 sudo curl -O https://dl.google.com/go/go1.12.linux-amd64.tar.gz
 
 sudo tar -xvf go1.12.linux-amd64.tar.gz
 sudo mv go /usr/local
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
-source ~/.profile
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bash_profile
+source ~/.bash_profile
 echo "GOLANG version ..."
 go version
-
-#GOPATH Directory
-cd $HOME
-mkdir go
-echo 'export GOPATH=$HOME/go' >> ~/.profile
-source ~/.profile
-
-echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.profile
-source ~/.profile
-
 
 #FABRIC
 # Ejecuta el setup inicial de las maquinas , creación de directorios y descarga de imagenes de Hyperledger Fabric
@@ -163,5 +151,5 @@ sudo chmod -R 777 $FABRICSamplesDir
 cd $FABRICSamplesDir
 sudo curl -sSL http://bit.ly/2ysbOFE | bash -s 2.2.0
 
-echo 'export PATH=$PATH:$HOME/hyperledger/fabric/fabric-samples/bin' >> ~/.profile
-source ~/.profile
+echo 'export PATH=$PATH:$HOME/hyperledger/fabric/fabric-samples/bin' >> ~/.bash_profile
+source ~/.bash_profile
